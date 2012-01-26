113383ad15e68821122c219e703bd86ff05519ef
atasatamatara <atasatamatara@gmail.com>
2012-01-26 16:48:26 +0900

    フォームまわりのCSSとか

--- yonda/forms.py
+++ yonda/forms.py
@@ -4,15 +4,17 @@ from django import forms
 
 from yonda.models import *
 
-class UrlPostForm(forms.ModelForm):
-    class Meta:
-        model = Url
-        fields = ('url',)
+class UrlPostForm(forms.Form):
+    url = forms.CharField(label=u"url", required=True, initial="http://",
+                          widget=forms.TextInput(attrs={"class":"url_postform"}))
 
 class BookmalkletForm(forms.Form):
-    title = forms.CharField(label=u"タイトル", required=True)
-    url = forms.CharField(label=u"url", required=True, initial="http://")
-    user= forms.CharField(label=u"名前", required=True, initial="増田")
+    title = forms.CharField(label=u"タイトル", required=True,
+                          widget=forms.TextInput(attrs={"class":"title_postform"}))
+    url = forms.CharField(label=u"url", required=True, initial="http://",
+                          widget=forms.TextInput(attrs={"class":"url_postform"}))
+    user= forms.CharField(label=u"名前", required=True, initial="増田",
+                          widget=forms.TextInput(attrs={"class":"name_postform"}))
 
 #class SingupFrom(forms.ModelForm):
 #    class Meta:
