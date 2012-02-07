d7f4878486a7353dc9548f4744e38324f3bc3f73
atasatamatara <atasatamatara@gmail.com>
2012-02-07 14:06:41 +0900

    UserAdminを再追加

--- yonda/admin.py
+++ yonda/admin.py
@@ -1,9 +1,14 @@
 from yonda.models import *
 from django.contrib import admin
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ctime',)
+    readonly_fields = ('ctime',)
+
 class UrlAdmin(admin.ModelAdmin):
     list_display = ('url', 'title', 'user', 'ctime',)
     readonly_fields = ('ctime',)
 
+admin.site.register(User, UserAdmin)
 admin.site.register(Url, UrlAdmin)
 
