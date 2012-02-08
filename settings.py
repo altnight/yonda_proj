a44e9913740a69766ceb23f2f474e862acc14af1
atasatamatara <atasatamatara@gmail.com>
2012-02-08 13:17:11 +0900

    gunicorn追加

--- settings.py
+++ settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'yonda',
+    'gunicorn',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
