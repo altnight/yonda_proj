a101fd70bc1db4c4ce60f4ceccbfd9d63965d88d
atasatamatara <atasatamatara@gmail.com>
2012-02-09 16:01:52 +0900

    bookmarklet fix

--- static/bookmarklet.js
+++ static/bookmarklet.js
@@ -4,4 +4,4 @@ javascript:(function(){
   open('http://127.0.0.1:8000/bookmarklet/add?title='+ encodeURIComponent(title) +'&url='+ encodeURIComponent(url),'yonda', 'width=512, height=384')
 })();
 
-javascript:(function(){var%20title=document.title;var%20url=location.href;open('http://127.0.0.1:8000/bookmarklet?title='+encodeURIComponent(title)+'&url='+encodeURIComponent(url),'yonda', 'width=512, height=384')})();
+javascript:(function(){var%20title=document.title;var%20url=location.href;open('http://127.0.0.1:8000/bookmarklet?title='+encodeURIComponent(title)+'&url='+encodeURIComponent(url),'yonda', 'width=600, height=480')})();
