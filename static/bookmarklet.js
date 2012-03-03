0b71e097717a2cf2f2b09396e4b2030f95237acc
atasatamatara <atasatamatara@gmail.com>
2012-03-03 21:43:18 +0900

    使ってないstaticcを除去

--- static/bookmarklet.js
+++
@@ -1,7 +0,0 @@
-javascript:(function(){
-  var title = document.title;
-  var url = location.href;
-  open('http://127.0.0.1:8000/bookmarklet/add?title='+ encodeURIComponent(title) +'&url='+ encodeURIComponent(url),'yonda', 'width=512, height=384')
-})();
-
-javascript:(function(){var%20title=document.title;var%20url=location.href;open('http://127.0.0.1:8000/bookmarklet?title='+encodeURIComponent(title)+'&url='+encodeURIComponent(url),'yonda', 'width=600, height=480')})();
